8a5aacf38f6022ecca200c979f8b5f3260f793ff
YJ <mqsy_yj@163.com>
2021-09-20 23:54:08 +0800

    设置angle组件

---
+++ fuckui/SetAngle.ts
@@ -0,0 +1,20 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import FuckUi from "./FuckUi";
+
+const {ccclass, menu} = cc._decorator;
+
+@ccclass
+@menu('NoUi/ui/SetAngle(设置旋转角度:number)')
+export default class SetAngle extends FuckUi {
+
+    protected onDataChange(data: any){
+        if (data == null) return;
+        this.node.angle = Number(data);
+    }
+}
