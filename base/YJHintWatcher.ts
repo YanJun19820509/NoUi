1a15361cd8644f4f9c398e450f6f4382a076fffd
YJ <mqsy_yj@163.com>
2021-09-20 23:52:48 +0800

    红点监听器组件

---
+++ base/YJHintWatcher.ts
@@ -0,0 +1,65 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import SetHint from "../fuckui/SetHint";
+import { no } from "../no";
+
+const { ccclass, property, menu } = cc._decorator;
+
+@ccclass
+@menu('NoUi/base/YJHintWatcher(红点监听)')
+export default class YJHintWatcher extends cc.Component {
+
+    @property({ type: SetHint })
+    hint: SetHint = null;
+
+    @property({ displayName: '红点key', tooltip: '多个key用逗号分隔' })
+    types: string = '';
+
+    private typeList: string[] = new Array();
+
+    onEnable() {
+        if (this.hint == null) return;
+        this.bind();
+    }
+
+    onDisable() {
+        no.hintCenter.offHint(this);
+    }
+
+    private bind() {
+        if (this.types == '') return;
+        let types = this.types.split(',');
+        this.typeList = types;
+        types.forEach(type => {
+            this.bindHint(type);
+        });
+    }
+
+    protected bindHint(type: string): void {
+        no.hintCenter.onHint(type, this.setHint, this);
+    }
+
+    protected setHint(v: number): void {
+        let n = 0;
+        if (this.hint.isNumber) {
+            this.typeList.forEach(type => {
+                n += no.hintCenter.getHintValue(type);
+            });
+        } else if (v < 1) {
+            let len = this.typeList.length;
+            for (let i = 0; i < len; i++) {
+                let type = this.typeList[i];
+                if (no.hintCenter.getHintValue(type) > 0) {
+                    n = 1;
+                    break;
+                }
+            }
+        } else n = 1;
+        this.hint.setData(n);
+    }
+}
