d1b904c08bec3e999b97ccf7e0d2c6c48bd51c6f
YJ <mqsy_yj@163.com>
2021-09-20 23:54:45 +0800

    设置color组件

---
+++ fuckui/SetColor.ts
@@ -0,0 +1,25 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import { no } from "../no";
+import FuckUi from "./FuckUi";
+
+const { ccclass, menu } = cc._decorator;
+
+@ccclass
+@menu('NoUi/ui/SetColor(设置颜色:string|cc.Color)')
+export default class SetColor extends FuckUi {
+
+    protected onDataChange(data: any) {
+        if (data == null) return;
+        if (typeof data == 'string') {
+            this.node.color = no.str2Color(data);
+        } else if (data instanceof cc.Color) {
+            this.node.color = data;
+        }
+    }
+}
