3f22170d31d5545dc25284d03c6261c22e3145db
YJ <mqsy_yj@163.com>
2021-09-23 23:58:23 +0800

    设置scrollview滚动到某节点或百分比位置

---
+++ fuckui/SetScrollToNode.ts
@@ -0,0 +1,36 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import SetScrollToPercent from "./SetScrollToPercent";
+
+const { ccclass, property, menu } = cc._decorator;
+/**
+ * scrollview滚动到某个节点
+ */
+@ccclass
+@menu('NoUi/ui/SetScrollToNode(设置scrollView滚动到节点:string)')
+export default class SetScrollToNode extends SetScrollToPercent {
+
+    protected onDataChange(data: any) {
+        this.a_scrollToNode(data);
+    }
+
+    public a_scrollToNode(name: string) {
+        if (this.scrollView == null) return;
+        let node = this.scrollView.content.getChildByName(name);
+        if (node == null) return;
+        let size = this.scrollView.content.getContentSize();
+        let rect = node.getBoundingBox();
+        let percent = 0;
+        if (this.scrollView.vertical) {
+            percent = -rect.origin.y / size.height;
+        } else {
+            percent = rect.origin.x / size.width;
+        }
+        this.a_scrollToPercent(percent);
+    }
+}
---
+++ fuckui/SetScrollToPercent.ts
@@ -0,0 +1,52 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import FuckUi from "./FuckUi";
+
+const { ccclass, property, menu } = cc._decorator;
+/**
+ * scrollview滚动到百分比位置
+ */
+@ccclass
+@menu('NoUi/ui/SetScrollToPercent(设置scrollView滚动到:number)')
+export default class SetScrollToPercent extends FuckUi {
+
+    @property(cc.ScrollView)
+    scrollView: cc.ScrollView = null;
+
+    @property({ displayName: '目标位置在可视范围内%', min: 0, max: 1 })
+    at: number = 0.5;
+
+    @property({ displayName: '滚动动画时长(秒)', min: 0 })
+    duration: number = 0;
+
+    protected onDataChange(data: any) {
+        this.a_scrollToPercent(data);
+    }
+
+    public a_scrollToPercent(per: number) {
+        if (this.scrollView == null) return;
+        let size = this.scrollView.content.getContentSize();
+        let offset = cc.v2();
+        if (this.scrollView.vertical) {
+            offset.y = size.height * per - this.scrollView.node.height * this.at;
+        } else {
+            offset.x = size.width * per + this.scrollView.node.width * this.at;
+        }
+        this.scrollToOffset(offset);
+    }
+
+    protected scrollToOffset(offset: cc.Vec2) {
+        if (this.scrollView == null) return;
+        let maxOffset = this.scrollView.getMaxScrollOffset();
+        if (offset.x < 0) offset.x = 0;
+        else if (offset.x > maxOffset.x) offset.x = maxOffset.x;
+        if (offset.y < 0) offset.y = 0;
+        else if (offset.y > maxOffset.y) offset.y = maxOffset.y;
+        this.scrollView.scrollToOffset(offset, this.duration);
+    }
+}
