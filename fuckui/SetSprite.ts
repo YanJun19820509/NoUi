817acf393652c66fac79e05fbc2764259ba27e20
YJ <mqsy_yj@163.com>
2021-09-26 23:36:49 +0800

    设置精灵

---
+++ fuckui/SetSprite.ts
@@ -0,0 +1,30 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import { no } from "../no";
+import FuckUi from "./FuckUi";
+
+const { ccclass, property, menu } = cc._decorator;
+
+@ccclass
+@menu('NoUi/ui/SetSprite(设置精灵:string)')
+export default class SetSprite extends FuckUi {
+
+    @property(cc.Sprite)
+    sprite: cc.Sprite = null;
+
+    onLoad() {
+        this.sprite = this.sprite || this.getComponent(cc.Sprite);
+    }
+
+    protected onDataChange(data: any) {
+        if (this.sprite == null) return;
+        no.assetBundleManager.loadSprite(String(data), spriteFrame => {
+            this.sprite.spriteFrame = spriteFrame;
+        });
+    }
+}
