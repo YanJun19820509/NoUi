545280f6b894512adf1b6f2779b6867b6ecdbce6
YJ <mqsy_yj@163.com>
2021-10-03 21:38:48 +0800

    按钮防连点及trigger

---
+++ fix/YJButton.ts
@@ -0,0 +1,46 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import { no } from "../no";
+
+const { ccclass, property, menu, requireComponent } = cc._decorator;
+
+@ccclass
+@menu('NoUi/fix/YJButton(防连点及trigger)')
+@requireComponent(cc.Button)
+export default class YJButton extends cc.Component {
+
+    @property({ displayName: '防连点间隔时长(s)' })
+    delay: number = 1;
+
+    private _waiting = false;
+    private _clickEvents: cc.Component.EventHandler[] = [];
+
+    onLoad() {
+        if (CC_EDITOR) return;
+        let btn = this.getComponent(cc.Button);
+        btn.clickEvents.forEach(e => {
+            this._clickEvents[this._clickEvents.length] = e;
+        });
+        let a = new cc.Component.EventHandler();
+        a.target = this.node;
+        a.component = 'YJButton';
+        a.handler = 'a_trigger';
+        btn.clickEvents = [a];
+    }
+
+    public a_trigger() {
+        if (this._waiting) return;
+        no.executeHandlers(this._clickEvents);
+        if (this.delay > 0) {
+            this._waiting = true;
+            this.scheduleOnce(() => {
+                this._waiting = false;
+            }, this.delay);
+        }
+    }
+}
