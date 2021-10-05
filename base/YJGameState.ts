de80701426af68277eb36b8268d26c2f1066b964
YJ <mqsy_yj@163.com>
2021-10-05 19:31:22 +0800

    监听游戏进入后台及回到前台的状态

---
+++ base/YJGameState.ts
@@ -0,0 +1,56 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import { no } from "../no";
+
+const { ccclass, property, menu } = cc._decorator;
+
+@ccclass
+@menu('NoUi/base/YJGameState(游戏状态)')
+export default class YJGameState extends cc.Component {
+
+    @property({ type: no.EventHandlerInfo, displayName: '进入后台时回调' })
+    onHideCalls: no.EventHandlerInfo[] = [];
+
+    @property({ type: no.EventHandlerInfo, displayName: '回到前台时回调' })
+    onShowCalls: no.EventHandlerInfo[] = [];
+
+    @property({ displayName: '进入后台多长时间执行回到前台回调(秒)' })
+    duration: number = 30;
+
+    @property({ displayName: '触发游戏重启的事件类型' })
+    type: string = 'game_restart'
+
+    private time: number;
+
+    onLoad() {
+        cc.game.on(cc.game.EVENT_HIDE, this.onHide, this);
+        cc.game.on(cc.game.EVENT_SHOW, this.onShow, this);
+        no.Evn.on(this.type, this.restart, this);
+    }
+
+    onDestroy() {
+        cc.game.targetOff(this);
+    }
+
+    private onHide(): void {
+        this.time = no.timestamp();
+        no.EventHandlerInfo.execute(this.onHideCalls);
+    }
+
+    private onShow(): void {
+        if (no.timestamp() - this.time >= this.duration)
+            no.EventHandlerInfo.execute(this.onShowCalls);
+    }
+
+    private restart() {
+        if (CC_JSB)
+            cc.game.restart();
+        else if (cc.sys.isBrowser)
+            window.document.location.reload();
+    }
+}
