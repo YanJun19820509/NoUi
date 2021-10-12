5d9b371aa8221f2bb814076d768518353cc4b248
YJ <mqsy_yj@163.com>
2021-10-12 23:21:41 +0800

    drawcall优化，按需创建和销毁动态合图

---
+++ engine/TmpAtlasManager.ts
@@ -0,0 +1,82 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+/**
+ * 临时动态合图管理器
+ */
+export class TmpAtlasManager {
+    private _excludes = [];
+    private _duration = 3;
+    private _canCreate = true;
+
+    private static _ins: TmpAtlasManager;
+    public static get ins(): TmpAtlasManager {
+        if (!this._ins) {
+            this._ins = new TmpAtlasManager();
+        }
+        return this._ins;
+    }
+
+    /**
+     *  设置不生成动态合图的类型
+     */
+    public set exclude(v: string | string[]) {
+        if (!v || v == '') return;
+        this._excludes = this._excludes.concat(v);
+    }
+
+    /**
+     * 设置创建动态合图的间隔时间:s
+     */
+    public set duration(v: number) {
+        if (v <= 0) return;
+        this._duration = v;
+    }
+
+    /**
+     * 临时动态合图数量
+     */
+    public get tmpAtlasNum(): number {
+        if (!this._enable) { return 0; }
+        return cc.dynamicAtlasManager['tmpAtlasNum'];
+    }
+    /**
+     * 创建临时动态合图
+     */
+    public create(type: string) {
+        if (!this._cd()) { return; }
+        if (!this._enable || this._excludes.includes(type)) { return; }
+        if (!cc.dynamicAtlasManager['tmpAtlasByName'](type)) {
+            cc.dynamicAtlasManager['setTmpAtlas'](type);
+            console.error(`setTmpAtlas${this.tmpAtlasNum}---${type}`);
+        }
+    }
+    /**
+     * 删除临时动态合图
+     */
+    public remove(type: string) {
+        if (!type || !this._enable || this._excludes.includes(type)) { return; }
+        console.error(`removeTmpAtlas${this.tmpAtlasNum}---${type}`);
+        cc.dynamicAtlasManager['removeTmpAtlas'](type);
+        console.error(`currentAtlasName---${cc.dynamicAtlasManager['currentAtlasName']}`);
+    }
+
+    private _cd(): boolean {
+        if (!this._canCreate) {
+            return false;
+        }
+        this._canCreate = false;
+        setTimeout(() => {
+            this._canCreate = true;
+        }, this._duration * 1000);
+        return true;
+    }
+
+    private get _enable(): boolean {
+        return cc.dynamicAtlasManager['setTmpAtlas'] != undefined && cc.dynamicAtlasManager.enabled;;
+    }
+}
