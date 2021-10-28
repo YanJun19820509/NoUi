bad301fa14423823c8ddf1a6c40dc2f6661abccf
YJ <mqsy_yj@163.com>
2021-10-28 21:17:13 +0800

    增加数据转YJDataWork组件

---
+++ fuckui/SetDataWork.ts
@@ -0,0 +1,25 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import YJDataWork from "../base/YJDataWork";
+import FuckUi from "./FuckUi";
+
+const { ccclass, property, menu } = cc._decorator;
+
+@ccclass
+@menu('NoUi/ui/SetDataWork(将数据赋值给YJDataWork:any)')
+export default class SetDataWork extends FuckUi {
+
+    @property(YJDataWork)
+    dataWork: YJDataWork = null;
+
+    protected onDataChange(data: any) {
+        if (this.dataWork) {
+            this.dataWork.data = data;
+        }
+    }
+}
