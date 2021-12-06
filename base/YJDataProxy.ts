976908336afb7ffb8cab7725b9614b45dc5be9b0
yj <mqsy_yj@163.com>
2021-12-06 18:10:13 +0800

    数据代理，支持多层级数据，支持动态增加数据层级，不支持数组

--- base/YJDataProxy.ts
+++ base/YJDataProxy.ts
@@ -5,33 +5,59 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import { no } from "../no";
 import YJDataWork from "./YJDataWork";
 import YJFuckUiRegister from "./YJFuckUiRegister";
 
-const { ccclass, property, menu, requireComponent } = cc._decorator;
+const { ccclass, menu, requireComponent } = cc._decorator;
 /**
- * YJDataProxy继承YJDataWork，提供一个代理，可直接通过.的方式来代替setValue方法，但仅支持第一层数据
+ * YJDataProxy继承YJDataWork，提供一个代理，可直接通过.的方式来代替setValue方法
  */
 @ccclass
 @menu('NoUi/base/YJDataProxy(数据代理基类)')
 @requireComponent(YJFuckUiRegister)
 export default class YJDataProxy extends YJDataWork {
 
-    private _proxy: any;
+    private _proxyMap: any = {};
 
-    public get proxy(): any {
-        if (!this._proxy) {
-            this._proxy = new Proxy(this._data.data, {
+    public get proxyData(): any {
+        if (!this._data) return null;
+        if (!this._proxyMap['__root']) {
+            this.setProxy(this._data.data, '__root');
+        }
+        return this._proxyMap['__root'];
+    }
+
+    private setProxy(d: any, key: string) {
+        if (d instanceof Array) return;
+        if (d instanceof Object) {
+            let me = this;
+            d['__path'] = key;
+            this._proxyMap[key] = new Proxy(d, {
                 get: function (target: any, property: string) {
-                    return target[property];
+                    let a = target[property];
+                    if (a && a['__path']) {
+                        return me._proxyMap[a['__path']] || a;
+                    }
+                    return a;
                 },
                 set: function (target: any, property: string, value: any): boolean {
-                    this.setValue(property, value);
+                    if (target[property] == undefined && value) {
+                        target[property] = value;
+                        me.setProxy(target, target['__path']);
+                    } else {
+                        if (value instanceof Object && !(value instanceof Array))
+                            me.setProxy(value, target[property]['__path']);
+                        target[property] = value;
+                    }
+
+                    me.setValue(`${target['__path']}.${property}`, value);
                     return true;
                 }
             });
-        }
-        return this._proxy;
+            for (const k in d) {
+                if (k == '__path') continue;
+                this.setProxy(d[k], key != '__root' ? `${key}.${k}` : k);
+            }
+        } else return;
     }
 }
