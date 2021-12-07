b77954e45246a12d7d27e9faadd73343f5f74fbf
yj <mqsy_yj@163.com>
2021-12-07 20:44:09 +0800

    设置shader属性参数组件

---
+++ base/shader/YJSetShaderProperties.ts
@@ -0,0 +1,57 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import SetShader from "../../fuckui/SetShader";
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass('DefineInfo')
+export class DefineInfo {
+    @property()
+    type: string = '';
+    @property()
+    isWork: boolean = false;
+}
+
+@ccclass('PropertyInfo')
+export class PropertyInfo {
+    @property()
+    type: string = '';
+    @property({ step: 0.01 })
+    value: number = 0.0;
+}
+
+
+@ccclass
+export default class YJSetShaderProperties extends cc.Component {
+    @property
+    path: string = '';
+
+    @property(DefineInfo)
+    defines: DefineInfo[] = [];
+
+    @property(PropertyInfo)
+    properties: PropertyInfo[] = [];
+
+    start() {
+        let ss = this.getComponent(SetShader);
+        let properties = {};
+        this.properties.forEach(p => {
+            properties[p.type] = p.value;
+        });
+        let defines = {};
+        this.defines.forEach(d => {
+            defines[d.type] = d.isWork;
+        });
+        let d = {
+            path: this.path,
+            properties: properties,
+            defines: defines
+        };
+        ss.setData(JSON.stringify(d));
+    }
+}
